97d719bf8d8274fd780a8a21e665cdbb1b7102c5
Babatunde oluwatomisin <Legendary@LEGENDASMACBOOK.lan>
2020-07-21 19:11:41 +0100

    set up complete

---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-banner>
+    order your foodstuff and get it delivered to your doorstep
+    <template v-slot:actions>
+      <v-btn text color="primary">products</v-btn>
+      <v-btn text color="warning">contact us</v-btn>
+    </template>
+  </v-banner>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <v-card>
+        <v-card-title class="headline">Welcome to Farmbase</v-card-title>
+        <v-card-text>
+          <p>Farmbase is being developed and maintained by Tomisinteazer and is currently under construction</p>
+
+          <div class="text-xs-right">
+            <em>
+              <small>&mdash; Tomisinteazer.</small>
+            </em>
+          </div>
+          <hr class="my-3" />
+          <a
+            href="https://tomisinteazer-io.web.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Tomisinteazer.io</a>
+          <br />
+          <a
+            href="https://github.com/nuxt/tomisinteazer"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Tomisinteazer GitHub</a>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" nuxt to="/inspire">Continue</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  components: {}
+};
+</script>
