1c5d8b74d1d9eb842bb60b50ea0961c95f2453e5
Tomisinteazer <tomisinteazer@.com>
2020-10-11 16:15:48 +0100

    about page edits.

--- pages/about.vue
+++ pages/about.vue
@@ -1,62 +1,64 @@
 <template>
-  <div class="pb-6 pt-4 text-center">
-    <v-container>
-      <div class="mb-10">
-        <h1 class="display-2 mb-6 text-uppercase primary--text">About us</h1>
-        <div class="mx-auto text-left" style="max-width: 700px">
-          <p class="body-1">
-            SwegzExchange Enterprise (formerly Aces Enterprise) is a financial
-            solutions firm birthed on 2nd of October 2019. The brand in a bid to
-            serve people better has been restructured and registered with the
-            Corporate Affairs Commission (CAC) for ease of operations.
-          </p>
-        </div>
-        <h1 class="mb-4 mx-auto text-h5 primary--text">Who are we</h1>
-        <div class="mx-auto text-left" style="max-width: 700px">
-          <p class="body-1">
-            We are a registered Nigerian firm (RC: 3152773) with expertise and
-            experience in finance. We deal majorly in the foreign exchange
-            (forex) market and the cryptocurrency world. We are also in
-            partnerships with prominent Trade companies with the sole purpose of
-            maximizing the benefits our investors relish. With a dedicated team
-            of experts, we are continuously working on better Trade
-            opportunities to enrich the lives of our investors
-          </p>
-        </div>
-        <h1 class="mb-4 mx-auto text-h5 primary--text">What we offer</h1>
-        <div class="mx-auto text-left" style="max-width: 700px">
-          <p class="body-1">
-            An Trade platform that gives people with little or no knowledge
-            about the forex markets an opportunity to earn from it. Our Trade
-            platform offers monthly returns on any amount invested with us.
-          </p>
-          <p class="body-1">
-            A cryptocurrency exchange platform that offers value for virtual
-            coins.
-          </p>
-        </div>
+  <div class="text-center">
+    <div class="primary py-16">
+      <h1
+        class="display-1 mb-6 pb-8 text-uppercase white--text font-weight-black"
+      >
+        About us
+      </h1>
+      <div class="mx-auto text-left" style="max-width: 700px">
+        <p class="text-h5 mb-0 pb-16 text-center white--text font-weight-bold">
+          SwegzExchange- Upgrading the world to a better trading system
+        </p>
       </div>
+    </div>
+    <svg viewBox="0 0 1440 84" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M0 8.84V0h1440v9.471C1208.167 58.315 967.825 84 721.497 84 475.423 84 235.324 58.368 3.713 9.623 2.475 9.363 1.237 9.1 0 8.839z"
+        fill="#690018"
+        fill-rule="evenodd"
+      />
+    </svg>
 
-      <v-divider></v-divider>
-      <div class="my-10">
-        <h2 class="text-h5 primary--text">COMPANY’S WATCHWORD</h2>
-        <p class="body-1">“Building Wealth, Creating Value.”</p>
-        <h2 class="text-h5 primary--text">OUR VISION</h2>
-        <p class="body-1">
-          To be a global high-ranking multi-faceted brand that creates value.
-        </p>
-        <h2 class="text-h5 primary--text">OUR MISSION</h2>
-        <p class="body-1">
-          At SwegzExchange, we believe in our cause to financially liberate the
-          new generation and financially empower as much people as we can with
-          our Trade platform and financial expertise.
-        </p>
-        <h2 class="text-h5 primary--text">OUR CORE VALUES</h2>
-        <p class="body-1">Innovation</p>
-        <p>Integrity</p>
-        <p>Accountability.</p>
-      </div>
-      <v-divider></v-divider>
-    </v-container>
+    <div class="mx-auto text-center my-6 py-10" style="max-width: 1000px">
+      <p class="text-h6 font-weight-regular">
+        We are a registered Nigerian firm (RC: 3152773) with expertise and
+        experience in finance. We deal majorly in the foreign exchange (forex)
+        market and the cryptocurrency world.
+      </p>
+    </div>
+    <v-divider class="mx-auto" style="max-width: 1000px"></v-divider>
+    <h1 class="mb-4 mx-auto text-h5 primary--text">What we offer</h1>
+    <div class="mx-auto text-left" style="max-width: 1000px">
+      <p class="body-1">
+        An Trade platform that gives people with little or no knowledge about
+        the forex markets an opportunity to earn from it. Our Trade platform
+        offers monthly returns on any amount invested with us.
+      </p>
+      <p class="body-1">
+        A cryptocurrency exchange platform that offers value for virtual coins.
+      </p>
+    </div>
+
+    <v-divider></v-divider>
+    <div class="my-10">
+      <h2 class="text-h5 primary--text">COMPANY’S WATCHWORD</h2>
+      <p class="body-1">“Building Wealth, Creating Value.”</p>
+      <h2 class="text-h5 primary--text">OUR VISION</h2>
+      <p class="body-1">
+        To be a global high-ranking multi-faceted brand that creates value.
+      </p>
+      <h2 class="text-h5 primary--text">OUR MISSION</h2>
+      <p class="body-1">
+        At SwegzExchange, we believe in our cause to financially liberate the
+        new generation and financially empower as much people as we can with our
+        Trade platform and financial expertise.
+      </p>
+      <h2 class="text-h5 primary--text">OUR CORE VALUES</h2>
+      <p class="body-1">Innovation</p>
+      <p>Integrity</p>
+      <p>Accountability.</p>
+    </div>
+    <v-divider></v-divider>
   </div>
 </template>
